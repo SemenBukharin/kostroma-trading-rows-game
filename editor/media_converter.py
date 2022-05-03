d7ddc9797d7aa36c12a4f1db81ca2e8a41d6089d
SemenBukharin <s.bukharin@mail.ru>
2022-05-03 19:04:57 +0300

    upd методы для изменения разрешения видео/фото

--- editor/media_converter.py
+++ editor/media_converter.py
@@ -1,5 +1,6 @@
 from moviepy.editor import *
 import cv2
+from PIL import Image, ImageSequence
 
 def changeVideoResolution(path, resolution, newName): 
     # путь к файлу, кортеж - разрешение (напр. (480, 480)), новое имя (с расширением файла)
@@ -12,3 +13,21 @@ def changeImageResolution(path, resolution, newName):
     image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     result = cv2.resize(image, resolution)
     cv2.imwrite(newName, result)
+    
+def changeGIFResolution(path, resolution, newName):     
+    gif = Image.open(path)
+    frames = ImageSequence.Iterator(gif)
+    frames = thumbnails(frames, resolution)
+    om = next(frames) # Handle first frame separately
+    om.info = gif.info # Copy sequence info
+    om.save(newName, save_all=True, append_images=list(frames), loop=0)
+    
+def thumbnails(frames, resolution): # вспомогательная функция
+    # Output (max) size
+    #size = 320, 240
+    #size = resolution
+    for frame in frames:
+        thumbnail = frame.copy()
+        #thumbnail.thumbnail(size, Image.ANTIALIAS)
+        thumbnail = thumbnail.resize(resolution)
+        yield thumbnail
