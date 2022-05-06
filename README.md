ccf1504de43f46d4f0ad5acb534cc77e0821bb1f
IvanTselikov <ivantselikov@yandex.ru>
2022-05-07 00:51:12 +0300

    Web Pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# kostroma-trading-rows-game
-Ссылка на бота: t.me/TraidingRowsGameBot
--- editor/server.py
+++
@@ -1,8 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-PORT = 8000
-
-if __name__ == '__main__':
-    server_address = ("", PORT)
-    httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-    httpd.serve_forever()
